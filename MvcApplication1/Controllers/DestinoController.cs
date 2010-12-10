f070f930e88ab3eb756b7049ad2eab29d1973869
juanmanuelinf <juanmanuelinf@gmail.com>
2010-12-09 23:08:53 -0430

    Destino con fotos de Flickr

--- MvcApplication1/Controllers/DestinoController.cs
+++ MvcApplication1/Controllers/DestinoController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using MvcApplication1.Dominio;
 using MvcApplication1.Dominio.Repositorios;
 using MvcApplication1.Dominio.Model;
+using FlickrNet;
 
 namespace MvcApplication1.Controllers
 {
@@ -23,6 +24,74 @@ namespace MvcApplication1.Controllers
         public ISet<Comentario> Comentarios = null;
         public Viaje Viaje = null;
 
+
+
+        static PhotoCollection photos = new PhotoCollection();
+        public ActionResult Create()
+        {
+            ViewData["Message"] = "Buscar destinos:";
+
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            var mapRepository = new MapRepository();
+            var map = new Map();
+            if (photos.Count == 0)
+                map = mapRepository.SetRepositories();
+            else
+                map = mapRepository.SetRepositories(photos);
+            return Json(map, JsonRequestBehavior.AllowGet);
+        }
+
+
+        
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Destino Destino,string Button)
+        {
+            if (Button == "Agregar Destino")
+            {
+                IRepositorio<Destino> repo = new DestinoRepositorio();
+                repo.Save(Destino);
+
+                return RedirectToAction("Index");
+            }
+            else {
+
+                var map = new Map();
+                UpdateModel(map);
+                int i = 0;
+                PhotoSearchOptions options = new PhotoSearchOptions();
+                //options.BoundaryBox = new BoundaryBox(-1.7, 54.9, -1.4, 55.2); // Roughly Newcastle upon Type, England
+                //options.BoundaryBox = BoundaryBox.World;
+                options.Extras |= PhotoSearchExtras.Geo;
+                options.Tags = map.Name;
+                options.HasGeo = true;
+                options.PerPage = 5;
+                Flickr flickr = new Flickr("3de826e278b4988011ef0227585a7838", "81a96df44a82b16c");
+                photos = flickr.PhotosSearch(options);
+                foreach (Photo photo in photos)
+                {
+                    ViewData["Message"] = String.Format("Lugares de \"{0}\".", map.Name);
+                    ViewData.Add(("Message" + i), photo.ThumbnailUrl);
+                    i++;
+                }
+
+                return View();
+            
+            }
+        }
+        
+     
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+
+   
         //
         // GET: /Destino/
         public ActionResult Index(int id,String nada)
@@ -37,10 +106,7 @@ namespace MvcApplication1.Controllers
             return View(repo.GetById(id));
         }
 
-        public ActionResult Create()
-        {
-            return View();
-        }
+       
 
         public ActionResult Edit(int id)
         {
@@ -54,15 +120,6 @@ namespace MvcApplication1.Controllers
 
             IRepositorio<Destino> repo = new DestinoRepositorio();
             repo.Update(Destino);
-
-            return RedirectToAction("Index");
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Create(Destino Destino)
-        {
-            IRepositorio<Destino> repo = new DestinoRepositorio();
-            repo.Save(Destino);
 
             return RedirectToAction("Index");
         }
--- MvcApplication1/Views/Destino/Create.aspx
+++ MvcApplication1/Views/Destino/Create.aspx
@@ -5,6 +5,31 @@
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+ <div id="map" style="width : 700px; height : 400px; margin : 0px; padding : 0px; float : left; margin-right:20px;"></div>
+    
+    <h2><%: ViewData["Message"] %></h2>
+    
+    <%Html.BeginForm();{%>
+    <div>
+    <fieldset>
+     <p>
+       <label for="Name">Lugar:</label>
+       <%= Html.TextBox("Name")%>
+     </p>
+     <input type="submit" value="Buscar" name="Button"/>
+    </fieldset>
+    </div>
+    <% } %>
+    
+    <p id="info"></p>
+    <div>
+     <% for (int i = 0; i < ViewData.Count-1; i++)
+        {%>
+        <img id="image" src="<%: ViewData["Message"+i] %>" alt=""/>
+     <% } %>
+     </div>
+    <div style="clear:both;"></div>
  <script src="../../Scripts/jquery-1.4.3.js"></script>
     <script src="../../Scripts/jquery.ui.core.js"></script>
     <script src="../../Scripts/jquery.ui.datepicker-es.js"></script>
@@ -18,38 +43,38 @@
       <form id="form1" runat="server">
     <h2>Create</h2>
 
-    <% using (Html.BeginForm()) {%>
-        <%: Html.ValidationSummary(true) %>
+    <% Html.BeginForm();{%>
+        <%: Html.ValidationSummary(true)%>
 
         <fieldset>
             <legend>Fields</legend>
             
             <div class="editor-label">
-                <%: Html.LabelFor(model => model.Descripcion) %>
+                <%: Html.LabelFor(model => model.Descripcion)%>
             </div>
             <div class="editor-field">
-                <%: Html.TextBoxFor(model => model.Descripcion) %>
-                <%: Html.ValidationMessageFor(model => model.Descripcion) %>
+                <%: Html.TextBoxFor(model => model.Descripcion)%>
+                <%: Html.ValidationMessageFor(model => model.Descripcion)%>
             </div>
             
             <div class="editor-label">
-                <%: Html.LabelFor(model => model.Direccion) %>
+                <%: Html.LabelFor(model => model.Direccion)%>
             </div>
             <div class="editor-field">
-                <%: Html.TextBoxFor(model => model.Direccion) %>
-                <%: Html.ValidationMessageFor(model => model.Direccion) %>
+                <%: Html.TextBoxFor(model => model.Direccion)%>
+                <%: Html.ValidationMessageFor(model => model.Direccion)%>
             </div>
             
             <div class="editor-label">
-                <%: Html.LabelFor(model => model.Fecha) %>
+                <%: Html.LabelFor(model => model.Fecha)%>
             </div>
             <div class="editor-field">
-                <%: Html.TextBoxFor(model => model.Fecha) %>
-                <%: Html.ValidationMessageFor(model => model.Fecha) %>
+                <%: Html.TextBoxFor(model => model.Fecha)%>
+                <%: Html.ValidationMessageFor(model => model.Fecha)%>
             </div>
             
             <p>
-                <input type="submit" value="Create" />
+                <input type="submit" value="Agregar Destino" name="Button" />
             </p>
         </fieldset>
 
