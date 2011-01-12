90c6836825e6b39d56f307141ad66a9f608b2fa2
Joseph Villalba <joseph.villalba@gmail.com>
2011-01-12 00:52:27 -0430

    Servicios Rest

---
+++ MvcApplication1/Dominio/Model/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Dominio.Model
+{
+    public class Pizza
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
